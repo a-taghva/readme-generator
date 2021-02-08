a3337afee31cb90721d62c9986bd0678a0887acf
a-taghva <taghvali99@gmail.com>
2021-02-07 20:42:38 -0500

    generate markdown

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -91,7 +92,7 @@ const questions = [
 ];
 
 inquirer.prompt(questions)
-    .then(data =>  console.log(data));
+    .then(data =>  generateMarkdown(data));
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
